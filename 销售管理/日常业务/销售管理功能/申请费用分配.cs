63fb16f554d83d1aedc771a3f98ae5f575c52229
weixinhuhu <weixin@rthitech.com.cn>
2019-03-22 09:29:48 +0800

    20190322

--- 销售管理/日常业务/销售管理功能/申请费用分配.cs
+++ 销售管理/日常业务/销售管理功能/申请费用分配.cs
@@ -1,10 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
 using System.Windows.Forms;
 using 销售管理.DAL.DataSetUsersTableAdapters;
 using 销售管理.DAL.DataSetExpenseAllocationTableAdapters;
